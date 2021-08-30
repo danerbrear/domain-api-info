8e1f4b59f93b50338b56ae5465fdaf0a4b6e9255
Dane Brear <dane.brear@gmail.com>
2021-08-30 12:09:38 -0700

    One command and file to start containers

--- handlerContainer/src/dns-service.ts
+++ handlerContainer/src/dns-service.ts
@@ -2,12 +2,6 @@ import { HttpWrapper } from './httpWrapper';
 
 const BASE_URL: string = "http://domain-service:8082";
 
-// export const DnsService: (ip: string) => Promise<axios> = (ip) => {
-//     return axios.get(`${BASE_URL}?ip=${ip}`, (res) => {
-//         console.log("Domain service response: ", res);
-//     })
-// }
-
 export class DnsService extends HttpWrapper {
     constructor(ip: string) {
         super(BASE_URL, ip);
