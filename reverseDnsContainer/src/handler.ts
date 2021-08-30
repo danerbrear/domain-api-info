ce45bc4e93e2511d5457dff63943d9ec39681b18
Dane Brear <dane.brear@gmail.com>
2021-08-30 10:23:38 -0700

    reverse dns changes

--- reverseDnsContainer/src/handler.ts
+++ reverseDnsContainer/src/handler.ts
@@ -1,13 +1,37 @@
 import { Request, Response } from "express";
+import * as dns from 'dns';
 
-export const get = (req: Request, res: Response) => {
-    res.send({
-        statusCode: 200,
-        body: {
-            message: 'it work'
-        },
-        headers: {
-            'content-type': 'application/json'
-        }
-    })
+export const get = async (req: Request, res: Response) => {
+    const ip = req.query.ip as string;
+    console.log("IP Address: ", ip);
+
+    if (ip) {
+        dns.reverse(ip, (err, hostnames) => {
+            console.log(err, hostnames);
+            
+            if (!err) {
+                res.send({
+                    statusCode: 200,
+                    body: {
+                        domain: hostnames
+                    },
+                    headers: {
+                        'content-type': 'application/json'
+                    }
+                })
+            } else {
+                res.send({
+                    statusCode: 500,
+                    body: 'Received an error from reverse dns service.',
+                })
+            }
+        })
+    } else {
+        res.send({
+            statusCode: 400,
+            body: 'Bad request.',
+        })
+    }
+
+    
 }
